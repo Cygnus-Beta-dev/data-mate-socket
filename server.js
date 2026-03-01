9ffa3272d673b2f77bdd2581810dc6ca623a971b
Ravi Kumar <ravikr1995.dev@gmail.com>
2026-03-01 17:04:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
     credentials: true,
   },
-  transports: ["websocket"],
+  transports: ["websocket","polling"],
   pingTimeout: 60000,
   pingInterval: 25000,
 });
